ac835afd726a79647b108aac63d22fbedb886180
shelleypham <phamshelley@csu.fullerton.edu>
2018-02-24 17:33:36 -0800

    Added directory options

--- turtles_crawling.sh
+++ turtles_crawling.sh
@@ -5,16 +5,43 @@
 # a src folder for the specified python scripts, and run roscore. If turtlesim
 # is taken as an argument, it will run turtlesim.
 #
-# ./rospy_magic.sh [option] [arguments]
-# ./rospy_magic.sh -tf pyfile1.py pyfile2.py...
+# eg:
+# ./rospy_magic.sh [option] "<filename.1> <filename.2>...<filename.n>"
+#
+# Options:
+#	-t: turtlesim
+#	-d: directory
+#	-f: files
+#	-h: help
+#
+# ./rospy_magic.sh -f "pyfile1.py pyfile2.py"
 #
 # Note:
 # Make sure to run this script in the same directory as your python file
 
-while getopts 't' flag; do
+turtle=false
+directoryN=''
+files=''
+
+while getopts 'tf:d:' flag; do
 case ${flag} in
-	t) echo "Yay! It works!"
-			exit 0;;
-	*) error "Unexpected option ${flag}" ;;
+	t) turtle=true ;;
+	d) directory="${OPTARG}" ;;
+	f) files="${OPTARG}" ;;
+	*) error "Unexpected option ${flag}" exit 0;;
 esac
 done
+
+if $turtle = true; then
+	echo "=== Activated TurtleSim! ==="
+fi
+
+if [ "$directory" != '' ]; then
+	echo $directory
+fi
+
+if [ "$files" != '' ]; then
+for file in $files; do
+	echo $file
+done
+fi
